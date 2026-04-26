8f78bcd14cf2790f85d621cd19140a0b7e90030c
Alfonso <ps3rioseras007@gmail.com>
2026-04-26 08:33:18 +0200

    Add files via upload

--- inventario/script.js
+++ inventario/script.js
@@ -1,25 +1,25 @@
 document.addEventListener('DOMContentLoaded', () => {
 
     const images = [
-        { src: "batgirl.png", link: "https://www.printables.com/model/1625336-batgirl" },
-        { src: "blackwidow.png", link: "https://www.printables.com/model/1684304-black-widow" },
-        { src: "catwoman.png", link: "https://www.printables.com/model/1614733-catwoman" },
-        { src: "heroina.png", link: "https://www.printables.com/model/1690267-heroina-sexy" },
-        { src: "Hulka.png", link: "https://www.printables.com/model/1614690-hulka" },
-        { src: "Spider Woman.png", link: "https://www.printables.com/model/1617016-spider-woman" },
-        { src: "Bruja escarlata.png", link: "https://www.printables.com/model/1616997-bruja-escarlata" },
-        { src: "Poison Ivy.png", link: "https://www.printables.com/model/1675062-poison-ivy" },
-        { src: "Mamba negra.png", link: "https://www.printables.com/model/1696056-mamba-negra-tanya-sealy" },
-        { src: "Killer Frost.png", link: "https://www.printables.com/model/1673838-killer-frost" },
-        { src: "Susan Storm.png", link: "https://www.printables.com/model/1628621-susan-storm" },
-        { src: "Power girl.png", link: "https://www.printables.com/model/1659485-power-girl-18" },
-        { src: "Rogue.png", link: "https://www.printables.com/model/1621854-rogue" },
-        { src: "Saturngirl.png", link: "https://www.printables.com/model/1700066-saturn-girl" },
-        { src: "Sungirl.png", link: "https://www.printables.com/model/1680251-sungirl" },
+        { src: "../modelos/batgirl.png", link: "https://www.printables.com/model/1625336-batgirl" },
+        { src: "../modelos/blackwidow.png", link: "https://www.printables.com/model/1684304-black-widow" },
+        { src: "../modelos/catwoman.png", link: "https://www.printables.com/model/1614733-catwoman" },
+        { src: "../modelos/heroina.png", link: "https://www.printables.com/model/1690267-heroina-sexy" },
+        { src: "../modelos/Hulka.png", link: "https://www.printables.com/model/1614690-hulka" },
+        { src: "../modelos/Spider Woman.png", link: "https://www.printables.com/model/1617016-spider-woman" },
+        { src: "../modelos/Bruja escarlata.png", link: "https://www.printables.com/model/1616997-bruja-escarlata" },
+        { src: "../modelos/Poison Ivy.png", link: "https://www.printables.com/model/1675062-poison-ivy" },
+        { src: "../modelos/Mamba negra.png", link: "https://www.printables.com/model/1696056-mamba-negra-tanya-sealy" },
+        { src: "../modelos/Killer Frost.png", link: "https://www.printables.com/model/1673838-killer-frost" },
+        { src: "../modelos/Susan Storm.png", link: "https://www.printables.com/model/1628621-susan-storm" },
+        { src: "../modelos/Power girl.png", link: "https://www.printables.com/model/1659485-power-girl-18" },
+        { src: "../modelos/Rogue.png", link: "https://www.printables.com/model/1621854-rogue" },
+        { src: "../modelos/Saturngirl.png", link: "https://www.printables.com/model/1700066-saturn-girl" },
+        { src: "../modelos/Sungirl.png", link: "https://www.printables.com/model/1680251-sungirl" },
         { src: "Supergirl.png", link: "https://www.printables.com/model/1651267-supergirl-18" },
-        { src: "Titania.png", link: "https://www.printables.com/model/1666376-titania-18" },
-        { src: "Storm.png", link: "https://www.printables.com/model/1615981-tormenta-x-men-mutante" },
-        { src: "Wonder Woman.png", link: "https://www.printables.com/model/1613204-wonder-woman" }
+        { src: "../modelos/Titania.png", link: "https://www.printables.com/model/1666376-titania-18" },
+        { src: "../modelos/Storm.png", link: "https://www.printables.com/model/1615981-tormenta-x-men-mutante" },
+        { src: "../modelos/Wonder Woman.png", link: "https://www.printables.com/model/1613204-wonder-woman" }
     ];
 
     const gallery = document.getElementById('gallery');
